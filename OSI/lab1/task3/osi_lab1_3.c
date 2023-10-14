2ecbd5f8f7622098a5ec474792d7adc9b11c900e
OlyaSilaeva-dev <olya-dom2014@yandex.ru>
2023-10-14 16:43:09 +0300

    osi_lab1: 3

--- OSI/lab1/task3/osi_lab1_3.c
+++ OSI/lab1/task3/osi_lab1_3.c
@@ -6,18 +6,26 @@ int main(int argc,char* argv[]){
        return 1;
     }
 
-    FILE* file_input = fopen(argv[1], "r");
-    FILE* file_output = fopen(argv[2], "w");
-    if(file_input == NULL || file_output == NULL){
-        printf("file is NULL");
+    FILE* file_input = fopen(argv[1], "rb");
+    if(file_input == NULL){
+        printf("file1 is NULL");
+        return 2;
+    }
+
+    FILE* file_output = fopen(argv[2], "wb");
+    if(file_output == NULL){
+        printf("file2 is NULL");
         return 2;
     }
 
     char ch;
-    while(!feof(file_input)){
-        ch = getc(file_input);
-        putc(ch, file_output);
+    char buffer[1024];
+    size_t bytes_read;
+
+    while((bytes_read = fread(buffer, 1, sizeof(buffer), file_input)) > 0){
+        fwrite(buffer, 1, bytes_read, file_output);
     }
+
     fclose(file_input);
     fclose(file_output);
     return 0;
