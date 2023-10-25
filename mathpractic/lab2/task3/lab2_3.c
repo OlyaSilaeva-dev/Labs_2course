93d32524df812e0a242fb0b997871377892b72c4
OlyaSilaeva-dev <olya-dom2014@yandex.ru>
2023-10-25 12:14:28 +0300

    lab 2 task 3

--- mathpractic/lab2/task3/lab2_3.c
+++ mathpractic/lab2/task3/lab2_3.c
@@ -1,8 +1,39 @@
 #include<stdio.h>
 #include<stdarg.h>
 #include<math.h>
-#include<string.h>
 #define BUFSIZE 255
+
+const char* strstr(const char *str, const char *substr){
+    const char* str_ptr = NULL;
+    const char* substr_ptr = NULL;
+
+    if(str == NULL || substr == NULL){
+        return NULL;
+    }
+
+    while(*str){
+        if(*str == *substr){
+            int trig = 1;
+            str_ptr = str;
+            substr_ptr = substr;
+
+            while(*str_ptr && *substr_ptr){
+                trig = trig && (*str_ptr == *substr_ptr);
+                if(!trig){
+                    break;
+                }
+                ++str_ptr;
+                ++substr_ptr;
+            }
+
+            if(trig && !*substr_ptr){
+                return str;
+            }
+        }
+        ++str;
+    }
+    return NULL;
+}
 
 int searchInFile(const char *substring, const char *filePath) {
     FILE *file = fopen(filePath, "r");
@@ -14,7 +45,7 @@ int searchInFile(const char *substring, const char *filePath) {
     int lineNum = 0;
     while (fgets(line, sizeof(line), file)) {
         lineNum++;
-        char *pos = line;
+        const char *pos = line;
         while ((pos = strstr(pos, substring)) != NULL) {
             printf("File: %s - Line: %d, Position: %ld\n", filePath, lineNum, (pos - line) + 1);
             pos += 1;
