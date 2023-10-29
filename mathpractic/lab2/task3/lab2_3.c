d6043fe940981b8ea90c3696c59702132301e592
OlyaSilaeva-dev <olya-dom2014@yandex.ru>
2023-10-29 23:41:31 +0300

    lab2_3

--- mathpractic/lab2/task3/lab2_3.c
+++ mathpractic/lab2/task3/lab2_3.c
@@ -4,6 +4,23 @@
 #define BUFSIZE 255
 #define MAXFILES 10
 
+typedef struct SearchResult {
+    char file[BUFSIZE];
+    int line;
+    long position;
+} SearchResult;
+
+void my_strcpy(char *str1, const char *str2)
+{
+    int index = 0;
+    while (str2[index] != '\0')
+    {
+        str1[index] = str2[index];
+        index++;
+    }
+    str1[index] = '\0';
+}
+
 const char *str_str(const char *str, const char *substr)
 {
     const char *str_ptr = NULL;
@@ -43,7 +60,7 @@ const char *str_str(const char *str, const char *substr)
     return NULL;
 }
 
-int search_in_file(const char *substring, const char *file_path, char res[MAXFILES][BUFSIZE])
+int search_in_file(const char *substring, const char *file_path, SearchResult *result, int *index)
 {
     FILE *file = fopen(file_path, "r");
     if (file == NULL)
@@ -61,33 +78,30 @@ int search_in_file(const char *substring, const char *file_path, char res[MAXFIL
         const char *pos = line;
         while ((pos = str_str(pos, substring)) != NULL)
         {
-            printf("File: %s - Line: %d, Position: %ld\n", file_path, line_num, (pos - line) + 1);
-            pos += 1;
+            result[*index].line = line_num;
+            result[*index].position = (pos - line) + 1;
+            my_strcpy(result[*index].file, file_path);
+            (*index)++;
             found = 1;
+            pos++;
         }
     }
 
-    if (found)
-    {
-        strcpy(res[0], file_path);
-        return 0;
-    }
-
     fclose(file);
-    return 0;
+    return found ? 0 : -1;
 }
 
-int search_in_files(const char *substring, char res[MAXFILES][BUFSIZE], ...)
+int search_in_files(const char *substring, SearchResult *res, int* index, ...)
 {
     va_list file_paths;
     const char *current_path;
     int i = 0;
 
-    va_start(file_paths, res);
+    va_start(file_paths, index);
     while ((current_path = va_arg(file_paths, const char *)) != NULL)
     {
-        int ans_sif = search_in_file(substring, current_path, res);
-        if (ans_sif != 0)
+        int ans_sif = search_in_file(substring, current_path, res, index);
+        if (ans_sif == 1)
         {
             return ans_sif;
         }
@@ -99,20 +113,21 @@ int search_in_files(const char *substring, char res[MAXFILES][BUFSIZE], ...)
 
 int main()
 {
-    const char *substring = "string"; // Замените на вашу строку поиска
-    char res[MAXFILES][BUFSIZE];
+    const char *substring = "1111"; // Замените на вашу подстроку.
+    SearchResult res[MAXFILES];
+    int index = 0;
 
-    int ans = search_in_files(substring, res, "file3.txt", "file1.txt", "file2.txt", NULL);
-    if (ans != 0)
+    int ans = search_in_files(substring, res, &index, "file3.txt", "file1.txt", "file2.txt", NULL);
+    if (ans == 1)
     {
-        printf("file_opening_error");
+        printf("file_opening_error\n");
         return 1;
     }
 
     printf("Files where the string is found: \n");
-    for (int i = 0; res[i][0] != '\0'; i++)
+    for (int i = 0; i < index; i++)
     {
-        printf("%s\n", res[i]);
+        printf("File: %s - Line: %d, Position: %ld\n", res[i].file, res[i].line, res[i].position);
     }
 
     return 0;
