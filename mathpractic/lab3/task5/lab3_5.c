24deb75fc83e720847d9103c8c67db146e165498
OlyaSilaeva-dev <olya-dom2014@yandex.ru>
2023-11-10 23:14:56 +0300

    lab3: task5

--- mathpractic/lab3/task5/lab3_5.c
+++ mathpractic/lab3/task5/lab3_5.c
@@ -5,8 +5,7 @@
 #define MAX_STUDENT 500
 #define STRING_SIZE 100
 
-typedef struct Student
-{
+typedef struct Student {
     uint32_t id;
     char name[STRING_SIZE];
     char surname[STRING_SIZE];
@@ -14,13 +13,11 @@ typedef struct Student
     double grades[5];
 } Student;
 
-int findID(Student *students, int size, uint32_t id, Student *res, int *index)
-{
+
+int findByID(Student *students, int size, uint32_t id, Student *res, int *index) {
     int j = 0;
-    for (int i = 0; i < size; i++)
-    {
-        if (students[i].id == id)
-        {
+    for (int i = 0; i < size; i++) {
+        if (students[i].id == id) {
             res[j] = students[i];
             j++;
         }
@@ -29,13 +26,10 @@ int findID(Student *students, int size, uint32_t id, Student *res, int *index)
     return 0;
 }
 
-int findSurname(Student *students, int size, char *sname, Student *res, int *index)
-{
+int findBySurname(Student *students, int size, char *sname, Student *res, int *index) {
     int j = 0;
-    for (int i = 0; i < size; i++)
-    {
-        if (strcmp(students[i].surname, sname) == 0)
-        {
+    for (int i = 0; i < size; i++) {
+        if (strcmp(students[i].surname, sname) == 0) {
             res[j] = students[i];
             j++;
         }
@@ -44,13 +38,10 @@ int findSurname(Student *students, int size, char *sname, Student *res, int *ind
     return 0;
 }
 
-int findName(Student *students, int size, char *name, Student *res, int *index)
-{
+int findByName(Student *students, int size, char *name, Student *res, int *index) {
     int j = 0;
-    for (int i = 0; i < size; i++)
-    {
-        if (strcmp(students[i].name, name) == 0)
-        {
+    for (int i = 0; i < size; i++) {
+        if (strcmp(students[i].name, name) == 0) {
             res[j] = students[i];
             j++;
         }
@@ -59,13 +50,10 @@ int findName(Student *students, int size, char *name, Student *res, int *index)
     return 0;
 }
 
-int findGroup(Student *students, int size, char *group, Student *res, int *index)
-{
+int findByGroup(Student *students, int size, char *group, Student *res, int *index) {
     int j = 0;
-    for (int i = 0; i < size; i++)
-    {
-        if (strcmp(students[i].group, group) == 0)
-        {
+    for (int i = 0; i < size; i++) {
+        if (strcmp(students[i].group, group) == 0) {
             res[j] = students[i];
             j++;
         }
@@ -74,29 +62,25 @@ int findGroup(Student *students, int size, char *group, Student *res, int *index
     return 0;
 }
 
-int cmpid(const void *a, const void *b)
-{
+int cmpid(const void *a, const void *b) {
     Student *stu1 = (Student *)a;
     Student *stu2 = (Student *)b;
     return stu1->id - stu2->id;
 }
 
-int cmpsur(const void *a, const void *b)
-{
+int cmpsur(const void *a, const void *b) {
     Student *stu1 = (Student *)a;
     Student *stu2 = (Student *)b;
     return strcmp(stu1->surname, stu2->surname);
 }
 
-int cmpname(const void *a, const void *b)
-{
+int cmpname(const void *a, const void *b) {
     Student *stu1 = (Student *)a;
     Student *stu2 = (Student *)b;
     return strcmp(stu1->name, stu2->name);
 }
 
-int cmpgroup(const void *a, const void *b)
-{
+int cmpgroup(const void *a, const void *b) {
     Student *stu1 = (Student *)a;
     Student *stu2 = (Student *)b;
     return strcmp(stu1->group, stu2->group);
@@ -115,173 +99,298 @@ int average_mark(double marks[], int index, double *res)
     return 0;
 }
 
-int find_all_students(Student *students, int mas_size, FILE *file_output, char flag)
-{
-    for (int j = 0; j < mas_size; j++)
-    {
-        Student *res = (Student *)malloc(sizeof(Student) * MAX_STUDENT);
-        if (res == NULL)
-        {
-            printf("memory allocation error");
-            fclose(file_output);
-            return 3;
-        }
-        int index = 0;
-        if (flag == 'i')
-        {
-            findID(students, mas_size, students[j].id, res, &index);
-        }
-        else if (flag == 's')
-        {
-            findSurname(students, mas_size, students[j].surname, res, &index);
-        }
-        else if (flag == 'n')
-        {
-            findName(students, mas_size, students[j].name, res, &index);
-        }
-        else
-        {
-            findGroup(students, mas_size, students[j].group, res, &index);
-        }
-        for (int i = 0; i < index; i++)
-        {
-            fprintf(file_output, "%u %s %s %s", res[i].id, res[i].name, res[i].surname, res[i].group);
-            for (int k = 0; k < 5; k++)
-            {
-                fprintf(file_output, " %lf", res[i].grades[k]);
-            }
-            fprintf(file_output, "\n");
-        }
-        free(res);
-    }
-    return 0;
-}
 
-
-int main(int argc, char *argv[])
-{
-    if (argc < 2 || argc > 3)
-    {
-        printf("Wrong argc");
-        return 1;
-    }
+int inputStudents(char* argv[], Student * infa, int* mas_size) {
+    if (infa == NULL){ return 2; }
 
     FILE *file_input = fopen(argv[1], "r");
-    if (file_input == NULL)
-    {
-        printf("file_input is NULL");
-        return 2;
-    }
-
-    Student *infa = (Student *)malloc(sizeof(Student) * MAX_STUDENT);
-    if (infa == NULL)
-    {
-        printf("memory allocation error");
-        fclose(file_input);
-        return 3;
-    }
+    if (file_input == NULL){ return 3; }
 
     int i = 0;
-    while (fscanf(file_input, "%u %s %s %s %lf %lf %lf %lf %lf", &infa[i].id, infa[i].name, infa[i].surname, infa[i].group, &infa[i].grades[0], &infa[i].grades[1], &infa[i].grades[2], &infa[i].grades[3], &infa[i].grades[4]) == 9)
-    {
-        if (i >= MAX_STUDENT)
-        {
-            printf("too much students");
+    while (fscanf(file_input, "%u %s %s %s %lf %lf %lf %lf %lf", &infa[i].id, &infa[i].name, &infa[i].surname, &infa[i].group, &infa[i].grades[0], &infa[i].grades[1], &infa[i].grades[2], &infa[i].grades[3], &infa[i].grades[4]) == 9) {
+        if (i >= MAX_STUDENT) {
             fclose(file_input);
             return 4;
         }
         i++;
     }
-    int mas_size = i;
+    *mas_size = i;
 
     fclose(file_input);
+    return 0;
+}
 
-    /////
+int fileOutput(FILE** file_output, int argc, char* argv[]) {
+    // printf("file opening");
+    if (argc == 3) {
+        *file_output = fopen(argv[2], "w");
+        if (*file_output == NULL) {
+            return 2;
+        }
+    } else {
+        *file_output = fopen("file_output.txt", "w");
+        if (*file_output == NULL) {
+            return 2;
+        }
+    }
+    // printf("file open");
+    return 0;
+}
+
+int printfStudents(FILE* file_output, int mas_size, Student* infa) {
+    for (int i = 0; i < mas_size; i++) {
+        fprintf(file_output, "%u %s %s %s", infa[i].id, infa[i].name, infa[i].surname, infa[i].group);
+        printf("[%d] %u %s %s %s", i, infa[i].id, infa[i].name, infa[i].surname, infa[i].group);
+        for (int k = 0; k < 5; k++) {
+            fprintf(file_output, " %lf", infa[i].grades[k]);
+            printf(" %lf", infa[i].grades[k]);
+        }
+        fprintf(file_output, "\n");
+        printf("\n");
+    }
+    return 0;
+}
+
+int averangeScore(FILE* file_output, int mas_size, Student* infa){
+    Student* res = (Student*)malloc(sizeof(Student) * MAX_STUDENT);
+    double* averanges = (double*)malloc(sizeof(double) * mas_size);
+    if(res == NULL || averanges == NULL){
+        return 2;
+    }
+
+    double sum = 0;
+    for(int i = 0; i < mas_size; i++){
+        double sum1 = 0;
+        for(int k = 0; k < 5; k++){
+            sum1 += infa[i].grades[k];
+        }
+        averanges[i] = sum1 / 5.0;
+        sum += sum1;
+      
+    }
+   
+    double averange = sum / (double)(mas_size * 5);    
+    
+    int j = 0;
+    for(int i = 0; i < mas_size; i++){
+        if(averanges[i] > averange){
+            res[j] = infa[i];
+            j++;
+        }
+    }
+    
+    printfStudents(file_output, j, res);
+
+    free(res);
+    free(averanges);
+
+    return 0;
+}
+
+int searchBySomething(FILE* file_output, int mas_size, Student* students, char flag){
+    Student *res = (Student *)malloc(sizeof(Student) * MAX_STUDENT);
+    if (res == NULL) {
+        return 2;
+    }
+
+    int ind = 0;
+    switch (flag){ 
+        case 'i':
+            printf("enter the id: ");
+            int id;
+            if(scanf("%u", &id) != 1){
+                printf("Wrong id");
+                return 5;
+            }
+            int ans1 = findByID(students, mas_size, id, res, &ind); //search by ID
+            printfStudents(file_output, ind, res);
+            break;
+
+        case 's':
+            printf("enter the sername: ");
+            char sername[STRING_SIZE];
+            
+            if(scanf("%s", &sername) != 1){
+                printf("Wrong sername");
+                return 5;
+            }
+            printf("%s", sername);
+
+            int ans2 = findBySurname(students, mas_size, sername, res, &ind); //search by ID
+            printfStudents(file_output, ind, res);
+            
+            break;
+
+        case 'n':
+            printf("enter the name: ");
+            char name[STRING_SIZE];
+
+            if(scanf("%s", name) != 1){
+                printf("Wrong name");
+                return 5;
+            }
+            
+            int ans3 = findByName(students, mas_size, name, res, &ind); //search by ID
+            printfStudents(file_output, ind, res);
+            break;
+
+        case 'g':
+            printf("enter the group: ");
+            char group[STRING_SIZE];
+            if(scanf("%s", group) != 1){
+                printf("Wrong group");
+                return 5;
+            }
+            int ans4 = findByGroup(students, mas_size, group, res, &ind); //search by ID
+            printfStudents(file_output, ind, res);
+            break;
+        default:
+            return 6;
+    }
+
+    free(res);
+    return 0;
+}
+
+
+int sortBySomething(FILE* file_output, int mas_size, Student* students, char flag){
+        
+    switch (flag){
+        case 'i':
+            fprintf(file_output, "sort by ID:\n");
+            qsort(students, mas_size, sizeof(Student), cmpid);
+            break;
+        case 's':
+            fprintf(file_output, "\nsort by surname:\n");
+            qsort(students, mas_size, sizeof(Student), cmpsur);
+            break;
+        case 'n':
+            fprintf(file_output, "\nsort by name:\n");
+            qsort(students, mas_size, sizeof(Student), cmpname);
+            break;
+        case 'g':
+            fprintf(file_output, "\nsort by group:\n");
+            qsort(students, mas_size, sizeof(Student), cmpgroup);
+            break;
+        
+        default:
+            return 6;
+    }
+    printfStudents(file_output, mas_size, students);
+
+    return 0;
+}
+
+int main(int argc, char *argv[]) {
+    if (argc < 2 || argc > 3) {
+        printf("Wrong argc");
+        return 1;
+    }
+
+    Student *students = (Student *)malloc(sizeof(Student) * MAX_STUDENT);
+    int mas_size;
+    int ans_input = inputStudents(argv, students, &mas_size);
+    if(ans_input != 0) {
+        free(students);
+        if(ans_input == 2) {
+            printf("memory allocation error");
+        } else if (ans_input == 3) {
+            printf("file_input is NULL");
+        } else {
+            printf("too much students");
+        }
+        return ans_input;
+    }
 
     FILE *file_output;
-    if (argc == 3)
-    {
-        file_output = fopen(argv[2], "w");
-        if (file_output == NULL)
-        {
-            printf("file is NULL");
-            return 2;
-        }
-    }
-    else
-    {
-        file_output = fopen("file_output.txt", "w");
-        if (file_output == NULL)
-        {
-            printf("file is NULL");
-            return 2;
-        }
+    int ans_output = fileOutput(&file_output, argc, argv);
+    
+    if(ans_output != 0){
+        free(students);
+        printf("file_output is NULL");
+        return ans_output;
     }
 
-    int ans1 = find_all_students(infa, mas_size, file_output, 'i'); //search by ID
-    int ans2 = find_all_students(infa, mas_size, file_output, 's'); //search by surname
-    int ans3 = find_all_students(infa, mas_size, file_output, 'n'); //search by name
-    int ans4 = find_all_students(infa, mas_size, file_output, 'g'); //search by group
 
-    if (ans1 == 3 || ans2 == 3 || ans3 == 3 || ans4 == 3)
-    {
-        printf("memory allocation error");
+    printf("Select a function: \n");
+    printf("f: find students\n");
+    printf("s: sort students\n");
+    printf("a: the average score is above the average for all students\n");
+    
+    char flag1;
+    int resscanf = scanf("%c", &flag1);
+    if(resscanf != 1){
+        printf("Wrong input");
         fclose(file_output);
-        return 3;
+        free(students);
+        return 6;
     }
 
-    if (ans1 == 4 || ans2 == 4 || ans3 == 4 || ans4 == 4)
-    {
-        printf("file output error");
+    if (flag1 == 'f') {
+        printf("i: search by id\n");
+        printf("s: search by surname\n");
+        printf("n: search by name\n");
+        printf("g: search by group\n");
+        getchar();
+
+        char flag2;
+        int ans = scanf("%c", &flag2);
+        if(ans != 1){
+            printf("Wrong input");
+            fclose(file_output);
+            free(students);
+            return 6;
+        }
+        int resSearch = searchBySomething(file_output, mas_size, students, flag2);
+        if(resSearch != 0) {
+            if(resSearch == 2) {
+                printf("memory allocation error");
+            } else if (resSearch == 5) {
+                printf("Wrong input");
+            } else {
+                printf("Wrong flag");
+            }
+            free(students);
+            fclose(file_output);
+            return resSearch; 
+        }
+    } else if (flag1 == 's'){
+        printf("i: sort by id\n");
+        printf("s: sort by surname\n");
+        printf("n: sort by name\n");
+        printf("g: sort by group\n");
+        getchar();
+
+        char flag2;
+        int ans = scanf("%c", &flag2);
+        if(ans != 1){
+            printf("Wrong flag");
+            fclose(file_output);
+            free(students);
+            return 6;
+        }
+        int resSort = sortBySomething(file_output, mas_size, students, flag2);
+        if(resSort == 6){
+            printf("Wrong flag");
+            free(students);
+            fclose(file_output);
+            return resSort;
+        }
+   } else if(flag1 == 'a'){
+        int resAver = averangeScore(file_output, mas_size, students);
+        if(resAver != 0){
+            printf("memory allocation error");
+            free(students);
+            fclose(file_output);
+        }
+   } else {
+        printf("Wrong flag");
+        free(students);
         fclose(file_output);
-        return 3;
+        return 6;
     }
 
-    qsort(infa, mas_size, sizeof(Student), cmpid);
-    for (i = 0; i < mas_size; i++)
-    {
-        fprintf(file_output, "%u %s %s %s", infa[i].id, infa[i].name, infa[i].surname, infa[i].group);
-        for (int k = 0; k < 5; k++)
-        {
-            fprintf(file_output, " %lf", infa[i].grades[k]);
-        }
-        fprintf(file_output, "\n");
-    }
-
-    qsort(infa, mas_size, sizeof(Student), cmpsur);
-    for (i = 0; i < mas_size; i++)
-    {
-        fprintf(file_output, "%u %s %s %s", infa[i].id, infa[i].name, infa[i].surname, infa[i].group);
-        for (int k = 0; k < 5; k++)
-        {
-            fprintf(file_output, " %lf", infa[i].grades[k]);
-        }
-        fprintf(file_output, "\n");
-    }
-
-    qsort(infa, mas_size, sizeof(Student), cmpname);
-    for (i = 0; i < mas_size; i++)
-    {
-        fprintf(file_output, "%u %s %s %s", infa[i].id, infa[i].name, infa[i].surname, infa[i].group);
-        for (int k = 0; k < 5; k++)
-        {
-            fprintf(file_output, " %lf", infa[i].grades[k]);
-        }
-        fprintf(file_output, "\n");
-    }
-
-    qsort(infa, mas_size, sizeof(Student), cmpgroup);
-    for (i = 0; i < mas_size; i++)
-    {
-        fprintf(file_output, "%u %s %s %s", infa[i].id, infa[i].name, infa[i].surname, infa[i].group);
-        for (int k = 0; k < 5; k++)
-        {
-            fprintf(file_output, " %lf", infa[i].grades[k]);
-        }
-        fprintf(file_output, "\n");
-    }
-
-    free(infa);
+    free(students);
     fclose(file_output);
     return 0;
 }
