602b8c9c1184e52ef22880beed41c09f2f5f7778
OlyaSilaeva-dev <olya-dom2014@yandex.ru>
2023-12-09 10:25:16 +0300

    lab3: task10

--- mathpractic/lab3/task10/lab3_10.c
+++ mathpractic/lab3/task10/lab3_10.c
@@ -1,154 +1,142 @@
-#include <stdio.h>
-#include <stdlib.h>
+#include "../header.h"
 
-typedef struct TreeNode {
-    char Data;
-    struct TreeNode* parent;
-    struct TreeNode* son;
-    struct TreeNode* brother;
-} TreeNode;
+struct Node {
+    char data;
+    int childCount;
+    struct Node** children;
+};
 
-int createNode(char data, TreeNode** res) {
-    TreeNode* newNode = (TreeNode*)malloc(sizeof(TreeNode));
-    if (newNode == NULL) {  
-        return 1; 
+struct Node* add_node(char data) {
+    struct Node* newNode = (struct Node*)malloc(sizeof(struct Node));
+    if (newNode != NULL) {
+        newNode->data = data;
+        newNode->childCount = 0;
+        newNode->children = NULL;
     }
-    newNode->Data = data;
-    newNode->son = NULL;
-    newNode->brother = NULL;
-    *res = newNode;
-    return 0;
+    return newNode;
 }
 
-int buildTree(char* expression, int* index, TreeNode** res) {
-    TreeNode* root = NULL;
-
-    while (expression[*index] != '\0') {
-        if (expression[*index] == '(') {
-            (*index)++;
-            TreeNode* newNode;
-            int ansCreate = createNode(expression[*index], &newNode);
-            if (ansCreate != 0) { 
-                return ansCreate;
-            }
-            (*index)++;
-
-            int ansBuild = buildTree(expression, index, &newNode->son);
-            if (ansBuild != 0) { 
-                return ansBuild;
-            }
-
-            if (root == NULL) {
-                root = newNode;
-            } else {
-                TreeNode* temp = root;
-                while (temp->son != NULL) {
-                    temp = temp->son;
-                }
-                temp->son = newNode;
-            }
-            (*index)++;
-
-        } else if (expression[*index] == ')') {
-            (*index)++;
-            *res = root;
-            return 0;
-        } else if (expression[*index] == ',') {
-            (*index)++;
-            TreeNode* newNode;
-            int ansCreate = createNode(expression[*index], &newNode);
-            if (ansCreate != 0) { 
-                return ansCreate;
-            }
-            (*index)++;
-
-            int ansBuild = buildTree(expression, index, &newNode->brother);
-            if (ansBuild != 0) { 
-                return ansBuild;
-            }
-
-            if (root == NULL) {
-                root = newNode;
-            } else {
-                TreeNode* temp = root;
-                while (temp->brother != NULL) {
-                    temp = temp->brother;
-                }
-                temp->brother = newNode;
-            }
-            (*index)++;
-
-        } else {
-            TreeNode* newNode;
-            int ansCreate = createNode(expression[*index], &newNode);
-            if (ansCreate != 0) { 
-                return ansCreate;
-            }
-            (*index)++;
-            if (root == NULL) {
-                root = newNode;
-            } else {
-                TreeNode* temp = root;
-                while (temp->brother != NULL) {
-                    temp = temp->brother;
-                }
-                temp->brother = newNode;
-            }
-        }
+int add_child(struct Node* parent, struct Node* child) {
+    parent->children = realloc(parent->children, (parent->childCount + 1) * sizeof(struct Node*));
+    if (parent->children == NULL) {
+        return NO_MEMORY;
     }
-
-    *res = root;
-    return 0;
+    parent->children[parent->childCount] = child;
+    parent->childCount++;
+    return OK;
 }
 
-void printTree(struct TreeNode* root, int level) {
+void print_tree(struct Node* root, int level, FILE* fptr) {
     if (root != NULL) {
         for (int i = 0; i < level; i++) {
-            printf(" ");
+            fprintf(fptr, "   ");
         }
-        printf( "%c\n", root->Data);
-        if (root->son != NULL || root->brother != NULL) {
-            printTree(root->son, level + 1);
-            printTree(root->brother, level);
+        if(level == 0) {
+            fprintf(fptr, "[-%c\n", root->data);
+        } else {
+            fprintf(fptr, "\\-%c\n", root->data);
+        }
+        
+        for (int i = 0; i < root->childCount; i++) {
+            print_tree(root->children[i], level + 1, fptr);
         }
     }
 }
 
-int main() {
-    // if (argc != 3) {
-    //     printf("Usage: %s input_file output_file\n", argv[0]);
-    //     return 1;
-    // }
-
-    // FILE* inputFile = fopen(argv[1], "r");
-    // if (inputFile == NULL) {
-    //     perror("Error opening input file");
-    //     return 1;
-    // }
-
-    // FILE* outputFile = fopen(argv[2], "w");
-    // if (outputFile == NULL) {
-    //     perror("Error opening output file");
-    //     fclose(inputFile);
-    //     return 1;
-    // }
-
-    char expression[] = "A(B(E(G,T,R(W,Z)),F(L,M)),C)";  // Adjust the size as needed
-
-    
-        int index = 0;
-        struct TreeNode* root;
-        int ansBuild = buildTree(expression, &index, &root);
-        if (ansBuild != 0) {
-            printf( "Error building tree for expression: %s\n", expression);
-        } else {
-            printTree(root, 0);
-            printf("\n");
+void free_tree(struct Node* root) {
+    if (root != NULL) {
+        for (int i = 0; i < root->childCount; i++) {
+            free_tree(root->children[i]);
         }
-    
+        free(root->children);
+        free(root);
+    }
+}
 
-    // fclose(inputFile);
-    // fclose(outputFile);
+int tree_by_brackets(struct Node** res, char* seq) {
+    struct Node* root = add_node(seq[0]);
+    if(root == NULL) {
+        return NO_MEMORY;
+    }
+    struct Node* node;
+    struct Node* curr = root;
+    struct Node** parent_stack = (struct Node**)malloc(strlen(seq) * sizeof(struct Node*));
+    if(parent_stack == NULL) {
+        return NO_MEMORY;
+    }
 
-    return 0;
+    int count_node = 0, length = strlen(seq);
+    for (int i = 0; i < length; i++) {
+        if (seq[i] == '(') {
+            node = add_node(seq[i + 1]);
+            parent_stack[count_node] = curr;
+            count_node++;
+            if (add_child(curr, node) != OK) {
+                free(parent_stack);
+                return NO_MEMORY;
+            }
+            curr = node;
+        } else if (seq[i] == ',') {
+            node = add_node(seq[i + 1]);
+            if (add_child(parent_stack[count_node - 1], node) != OK) {
+                free(parent_stack);
+                return NO_MEMORY;
+            }
+            curr = node;
+        } else if (seq[i] == ')') {
+            parent_stack[count_node - 1] = NULL;
+            count_node--;
+        }
+    }
+
+    free(parent_stack);
+    (*res) = root;
+    return OK;
+}
+
+int main(int argc, char* argv[]) {
+    if (argc != 3) {
+        print_err(INVALID_ARGC);
+        return INVALID_ARGC;
+    }
+
+    FILE* inputFile = fopen(argv[1], "r");
+    if (inputFile == NULL) {
+        print_err(FILE_NOT_OPEN);
+        return FILE_NOT_OPEN;
+    }
+    FILE* outputFile = fopen(argv[2], "w");
+    if(outputFile == NULL) {
+        fclose(inputFile);
+        print_err(FILE_NOT_OPEN);
+        return FILE_NOT_OPEN;
+    }
+
+    char buffer[BUFSIZ];
+    while (fgets(buffer, sizeof(buffer), inputFile) != NULL) {
+        //Удаление символа новой строки из конца буфера
+        size_t length = strlen(buffer);
+        if (length > 0 && buffer[length - 1] == '\n') {
+            buffer[length - 1] = '\0';
+        }
+
+        struct Node* root;
+        int st = tree_by_brackets(&root, buffer);
+        if(st != OK) {
+            fclose(inputFile);
+            fclose(outputFile);
+            free_tree(root);
+            print_err(st);
+            return st;
+        }
+
+        print_tree(root, 0, outputFile);
+        fprintf(outputFile, "\n");
+        free_tree(root);
+    }
+
+    fclose(inputFile);
+    fclose(outputFile);
+
+    return OK;
 }
